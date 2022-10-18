1f9689f7946e59a3b4d4bdcffce3741d0a28b8b7
asakuramk <m@llll.jp>
2022-10-19 07:38:27 +0900

    change messages

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 var diceNum1 = Math.floor(Math.random() * 6) + 1;
 var diceNum2 = Math.floor(Math.random() * 6) + 1;
 if (diceNum1 > diceNum2) {
-  document.querySelector("h1").innerText = "1!";
+  document.querySelector("h1").innerText = "🚩 Play 1 Wins!";
 } else if (diceNum1 < diceNum2) {
-  document.querySelector("h1").innerText = "2";
+  document.querySelector("h1").innerText = "Player 2 Wins! 🚩";
 } else {
-  document.querySelector("h1").innerText = "d";
+  document.querySelector("h1").innerText = "引き分け！";
 }
 
 document.querySelectorAll("img")[0].setAttribute("src", "dice" + diceNum1 + ".png");
